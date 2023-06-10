50b39d41b1d21703f9c55f5576d3fb137af700ff
Mehedi Hasan <mehedih.dev@gmail.com>
2023-06-10 12:29:14 +0600

    set refetchOnWindowsFocus to False to prevent auto update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import Facts from './Component/facts'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import React from 'react';
+import Facts from './Component/facts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-  const client = new QueryClient({})
+  const client = new QueryClient({defaultOptions:{
+    // queriy= Read in CRUD (R)
+    // mutations= Create Update Delete in CRUD (CUD)
+    queries:{
+      refetchOnWindowFocus:false,
+    }
+  }});
   return (
     <div>
       <QueryClientProvider client={client}>
@@ -15,5 +21,7 @@ function App() {
         </Router>
       </QueryClientProvider>
     </div>
-  )
+  );
 }
+
+export default App;
--- src/Component/Facts.jsx
+++ src/Component/Facts.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import  Axios  from 'axios'
+import React from 'react'
 import { useQuery } from '@tanstack/react-query'
+import Axios from 'axios'
 
 function Facts() {
-   const {data, isLoading} = useQuery(['cat'], ()=>{
+   const {data: catData, isLoading, isError, refetch} = useQuery(['cat'], ()=>{
     return(
         Axios.get('https://catfact.ninja/fact')
         .then((res)=>res.data)
@@ -14,10 +14,19 @@ function Facts() {
         return <h1>Loading...</h1>
    }
 
+   if (isError){
+    return <h1>Sorry, there was an error</h1>
+   }
+
     return (
     <div className='catfacts'>
         <h1>Cat Facts 😸</h1>
-        <h2>{data?.fact}</h2>
+        <h2>{catData?.fact}</h2>
+        <button onClick={refetch}>
+            Update Data
+        </button>
     </div>
     )
-}
+} 
+
+export default Facts;
